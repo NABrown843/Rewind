f56f614f9f34747f5d63db2963d573cc31da974b
NABrown843 <87168340+NABrown843@users.noreply.github.com>
2022-04-13 21:12:16 -0400

    Rewind Version 1 Commit
    Finished JavaScript functions and tested application to confirm it is working properly.

--- js/site.js
+++ js/site.js
@@ -1,14 +1,32 @@
 // Get the string from the page
-function getValue(){
-    let 
+// controller function
+function getValue() {
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;
+    let revString = reverseString(userString);
+
+    displayString(revString);
 }
 
 // Reverse the string 
-function reverseString(){
+// logic function
+function reverseString(userString) {
+    let revString = [];
 
+    // reverse a string using a for loop
+    for (index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];
+    }
+
+    return revString;
 }
 
 // Display the reversed string to the user
-function displayString()}{
-    
+// View Function
+function displayString(reverseString) {
+    // write to the page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${reverseString}`;
+    // show the alert box
+    document.getElementById("alert").classList.remove("invisible");
 }
